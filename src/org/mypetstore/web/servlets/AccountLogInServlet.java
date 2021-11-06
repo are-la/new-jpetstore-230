e6cbc8a9e2fb9be97a21d61618c3f6da309f1838
limujiu <73785930+limujiu@users.noreply.github.com>
2021-11-06 22:25:50 +0800

    Add files via upload
    Add PIN

--- src/org/mypetstore/web/servlets/AccountLogInServlet.java
+++ src/org/mypetstore/web/servlets/AccountLogInServlet.java
@@ -28,23 +28,48 @@ public class AccountLogInServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String checkCode = request.getParameter("checkPIN");
         username = request.getParameter("username");
         password = request.getParameter("password");
-        service = new AccountService();
-        account = new Account();
-        account = service.getAccount(username,password);
-        if(account != null)
-        {
-            System.out.println("login account is not null and account is "+ account.getUsername()+" "+ account.getPassword());
-            HttpSession session = request.getSession();
-            session.setAttribute("account",account);
-            request.getRequestDispatcher(Main).forward(request,response);
+
+        HttpSession session = request.getSession();
+        String validateCode = (String) session.getAttribute("validateCode");
+        //删除验证码，这样返回时验证码就会刷新一次
+        session.removeAttribute("validateCode");
+        if(validateCode!=null && validateCode.equalsIgnoreCase(checkCode)){
+            service = new AccountService();
+            account = new Account();
+            account = service.getAccount(username,password);
+            if(account != null)
+            {
+                System.out.println("login account is not null and account is "+ account.getUsername()+" "+ account.getPassword());
+                session.setAttribute("account",account);
+                request.getRequestDispatcher(Main).forward(request,response);
+            }
+            else {
+                System.out.println("login account is null");
+                session.setAttribute("message",ErrorString);
+                request.getRequestDispatcher(SignOn).forward(request,response);
+            }
+        }else{
+            session.setAttribute("message","验证码错误！");
+            request.getRequestDispatcher(Error).forward(request,response);
         }
-        else {
-            System.out.println("login account is null");
-            HttpSession session = request.getSession();
-            session.setAttribute("message",ErrorString);
-            request.getRequestDispatcher(SignOn).forward(request,response);
-        }
+//        service = new AccountService();
+//        account = new Account();
+//        account = service.getAccount(username,password);
+//        if(account != null)
+//        {
+//            System.out.println("login account is not null and account is "+ account.getUsername()+" "+ account.getPassword());
+//            HttpSession session = request.getSession();
+//            session.setAttribute("account",account);
+//            request.getRequestDispatcher(Main).forward(request,response);
+//        }
+//        else {
+//            System.out.println("login account is null");
+//            HttpSession session = request.getSession();
+//            session.setAttribute("message",ErrorString);
+//            request.getRequestDispatcher(SignOn).forward(request,response);
+//        }
     }
 }
